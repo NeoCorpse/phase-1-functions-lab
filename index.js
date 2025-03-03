ca8d1399dd10c08e54ec634dacd16e85be76b755
Ian Wafula <neocorpse@proton.me>
2025-03-03 14:02:43 +0300

    Add functions to calculate relevant distances and price

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(val) {
+  return Math.abs(val - 42);
+}
+
+function distanceFromHqInFeet(val) {
+  return distanceFromHqInBlocks(val) * 264;
+}
+
+function distanceTravelledInFeet(val1, val2) {
+  return Math.abs(val1 - val2) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = distanceTravelledInFeet(start, destination);
+  let price = 0;
+  switch (true) {
+    case distance > 2500:
+      return "cannot travel that far";
+    case distance >= 2000:
+      price = 25;
+      break;
+    case distance >= 400:
+      price += 0.02 * (Math.min(distance, 2000) - 400);
+      break;
+  }
+  return price;
+}
